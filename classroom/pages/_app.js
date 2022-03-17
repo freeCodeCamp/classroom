eae70a4a892f6b7e109686c8dea407b21a2c14b8
EPeralta18 <eperalta@csumb.edu>
2022-03-17 16:27:21 -0700

    Changed file structure. Also added image popup when you sign in.

---
+++ classroom/pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react"
+
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
---
+++ classroom/pages/api/auth/[...nextauth].js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+})
