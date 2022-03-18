2ba5fb99fc576c490f28a5af5f2facfb17e2cb4d
EPeralta18 <eperalta@csumb.edu>
2022-03-17 19:44:17 -0700

    Changed README & index

--- classroom/pages/index.js
+++ classroom/pages/index.js
@@ -2,8 +2,29 @@ import Head from 'next/head'
 import Image from 'next/image'
 import AuthButton from '../components/AuthButton'
 import styles from '../styles/Home.module.css'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+export async function getServerSideProps() {
+  // Here is where (if our env file is missing in our web folder) our code fails. 
+  // There is no way to reach our DB link since we cannot access our env file that states where it is.
+  const classrooms = await prisma.Classroom.findMany();
+  const output = [];
+  for (let i=0; i < classrooms.length; i++){
+    output[i] = {
+      "classroomName": classrooms[i].classroomName,
+      "classroomId": classrooms[i].classroomId,
+      "description": classrooms[i].description,
+      "createdAt": JSON.stringify(classrooms[i].createdAt)
+    }
+  }
+  return {
+    props: { classrooms : output }
+  };
+}
+
+export default function Home({ classrooms }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,60 +33,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <AuthButton></AuthButton>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={styles.container}>
+        <div className='table'>
+          {classrooms.map(classrooms => (
+            <div key={classrooms.id}>
+              <a>
+                <h3>{classrooms.classroomName}, created on: {classrooms.createdAt}</h3>
+              </a>
+            </div>
+          ))}
         </div>
-      </main>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <AuthButton></AuthButton>
     </div>
   )
 }
