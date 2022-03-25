04b561007d162776f94b9831f9d3fba2de007c31
egall18 <erikgallard123@gmail.com>
2022-03-24 20:13:18 -0700

    removing not used variable

---
+++ pages/invite/index.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Invite.module.css';
+
+export default function Invite() {
+  return (
+    <div className={styles.box}>
+      <input type='text'></input>
+    </div>
+  );
+}
