14947a8f2a06011ec825b8c8b0f1781fa140e5f4
GuillermoFloresV <guillermo.fifth@gmail.com>
2022-03-25 15:06:35 -0700

    Rebase with main

--- components/navbar.js
+++ components/navbar.js
@@ -4,16 +4,27 @@ import Image from 'next/image';
 export default function Navbar({ children }) {
   return (
     <div className={navbarStyles.navbar}>
-      <Image
-        className={navbarStyles.navimage}
-        priority
-        layout='fixed'
-        src='/images/fcc_primary_large.png'
-        alt='FreeCodecamp Logo'
-        width={330}
-        height={40}
-      ></Image>
-      <ul className={navbarStyles.navLinks}>{children}</ul>
+      <div className={navbarStyles.container}>
+        <div></div>
+        <div className={navbarStyles.logo}>
+          <Image
+            className={navbarStyles.navimage}
+            priority
+            layout='fixed'
+            src='/images/fcc_primary_large.png'
+            alt='FreeCodecamp Logo'
+            width={330}
+            height={40}
+          ></Image>
+        </div>
+        <div>
+          {children.map(children => (
+            <div key={children}>
+              <p>{children}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
