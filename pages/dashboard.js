56190d6d80802cd5e9ace7b6f8679bdecc9826bc
Robert Huskins <37780741+roberthuskins@users.noreply.github.com>
2022-04-04 17:32:37 -0700

    add dynamic columns

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,8 +2,40 @@ import Head from 'next/head';
 import Layout from '../components/layout';
 import Link from 'next/link';
 import Navbar from '../components/navbar';
+import DashTable from '../components/dashtable';
 
-export default function Home() {
+export async function getServerSideProps() {
+  let url = '';
+  const res = await fetch(url);
+  const req = await res.json();
+  const blocks = req['responsive-web-design']['blocks'];
+  let sortedBlocks = [];
+  for (var block in blocks) {
+    sortedBlocks.push([
+      { name: block, selector: null },
+      blocks[block]['challenges']['order']
+    ]);
+  }
+
+  //1 refers to the second element in our list
+  //https://lage.us/Javascript-Sort-2d-Array-by-Column.html
+
+  sortedBlocks.sort(function (a, b) {
+    if (a[1] === b[1]) {
+      return 0;
+    } else {
+      return a[1] < b[1] ? -1 : 1;
+    }
+  });
+
+  const arrayColumn = (arr, n) => arr.map(x => x[n]);
+
+  return {
+    props: { columns: arrayColumn(sortedBlocks, 0) }
+  };
+}
+
+export default function Home({ columns }) {
   return (
     <Layout>
       <Head>
@@ -16,6 +48,7 @@ export default function Home() {
           <Link href={'#'}>Sign out</Link>
         </Navbar>
       </Head>
+      <DashTable columns={columns}></DashTable>
     </Layout>
   );
 }
