15c21912e8041e891f15f1f01741f7e9dfea44ec
GuillermoFloresV <guillermo.fifth@gmail.com>
2022-04-12 18:44:46 -0700

    Added conditional styling based on selected tab

--- components/dashtable.js
+++ components/dashtable.js
@@ -1,22 +1,33 @@
 import DataTable from 'react-data-table-component';
+
+// Dummy data that we plan to create an endpoint for
 const data = [
   {
     id: 1,
     'Student Name': 'Guillermo',
-    'basic-html-and-html5': '10/10',
-    'basic-css': '0/10',
-    'css-flexbox': '10/10',
-    'css-grid': '7/10'
+    'Basic HTML and HTML5': '10/10',
+    'Basic CSS': '0/10',
+    'CSS Flexbox': '10/10',
+    'CSS Grid': '7/10',
+    'Regular Expressions': 'We need dummy data to populate this.',
+    Debugging: '0/10',
+    'Basic Data Structures': '4/10',
+    'Basic Algorithm Scripting': '10/10'
   },
   {
     id: 2,
     'Student Name': 'Robert',
-    'basic-css': 'We need dummy data to populate this.',
-    'basic-html-and-html5': '10/10',
-    'css-flexbox': '10/10',
-    'applied-accessibility': '2/10'
+    'Basic HTML and HTML5': '8/10',
+    'Basic CSS': '10/10',
+    'CSS Flexbox': '9/10',
+    'CSS Grid': '4/10',
+    'Regular Expressions': 'We need dummy data to populate this.',
+    Debugging: '10/10',
+    'Basic Data Structures': '10/10',
+    'Basic Algorithm Scripting': '10/10'
   }
 ];
+
 export default function DashTable(props) {
-  return <DataTable columns={props.columns} data={data} />;
+  return <DataTable columns={props.columns} data={data} pagination />;
 }
