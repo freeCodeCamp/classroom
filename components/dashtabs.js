4eff7ca951c8514e535b13158c18667f589fbc98
GuillermoFloresV <guillermo.fifth@gmail.com>
2022-04-12 18:44:46 -0700

    Added conditional styling based on selected tab

--- components/dashtabs.js
+++ components/dashtabs.js
@@ -1,32 +1,54 @@
-import Tabs from 'react-tabs/lib/components/Tabs';
-import Tab from 'react-tabs/lib/components/Tab';
 import DashTable from './dashtable';
-import { TabPanel } from 'react-tabs';
-import { TabList } from 'react-tabs';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import reactTabStyles from './dashTabs.module.css';
+import { useState } from 'react/cjs/react.development';
 
 export default function DashTabs(props) {
-  var arrayForConcat = [...props.columns];
-  for (let i = 0; i < arrayForConcat.length; i += 1) {
+  const [tabIndex, setTabIndex] = useState(0);
+  // This sets our selected tab to our first index of certification module names
+  const [tabIndexName, setTabIndexName] = useState(props.certificationNames[0]);
+  // Here we are copying the columns array (which is now immutable) in order to be able to add the Student Name column to it
+  var columnNames = [...props.columns];
+  // The outer loop corresponds to each individual certification
+  for (let i = 0; i < columnNames.length; i += 1) {
     let presetColumns = [];
     presetColumns.push({ name: 'Student Name', selector: 'Student Name' });
-    arrayForConcat[i] = presetColumns.concat(arrayForConcat[i]);
-    for (let k in Object.keys(arrayForConcat[i])) {
-      arrayForConcat[i][k]['selector'] = row =>
-        row[arrayForConcat[i][k]['name']];
+    columnNames[i] = presetColumns.concat(columnNames[i]);
+    // This inner loop adds the name of the column for each specific certification
+    for (let k in Object.keys(columnNames[i])) {
+      console.log(columnNames[i][k]);
+      columnNames[i][k]['selector'] = row => row[columnNames[i][k]['name']];
     }
   }
+  // This function sets the tab name which later gives our selected tab selected styling
+  function determineItemStyle(x) {
+    setTabIndexName(x);
+  }
+
   return (
-    <Tabs>
-      <TabList forceRenderTabPanel>
-        {props.certificationNames.map(x => (
-          <Tab key={x}>{x}</Tab>
+    <>
+      <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
+        <TabList>
+          {props.certificationNames.map(x => (
+            <Tab
+              onClick={() => determineItemStyle(x)}
+              className={
+                x == tabIndexName
+                  ? reactTabStyles.react_tabs__tab__selected
+                  : reactTabStyles.react_tabs__tab
+              }
+              key={x}
+            >
+              {x}
+            </Tab>
+          ))}
+        </TabList>
+        {columnNames.map(x => (
+          <TabPanel key={x}>
+            <DashTable columns={x} data={null}></DashTable>
+          </TabPanel>
         ))}
-      </TabList>
-      {arrayForConcat.map(arrayForConcat => (
-        <TabPanel key={1}>
-          <DashTable columns={arrayForConcat} data={null}></DashTable>
-        </TabPanel>
-      ))}
-    </Tabs>
+      </Tabs>
+    </>
   );
 }
