3c4d0210e9e5da978b21e4a5285322a3bca45b45
EPeralta18 <eperalta@csumb.edu>
2022-04-14 18:31:21 -0700

    Refactored authentication by modifying layout.js And added a join page layout for students.

--- pages/dashboard/[...classCode].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-
-export default function DashboardWithCode() {
-  const router = useRouter();
-  const { classCode } = router.query;
-
-  return <p>classCode: {classCode}</p>;
-}
