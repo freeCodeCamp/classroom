05650190e2d985e21f8aad4609ef72881bb41b35
EPeralta18 <eperalta@csumb.edu>
2022-04-14 18:31:21 -0700

    Refactored authentication by modifying layout.js And added a join page layout for students.

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,16 @@
+import { useSession } from 'next-auth/react';
+
 export default function Layout({ children }) {
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      return <div>onUnauthenticated</div>;
+    }
+  });
+
+  if (status === 'loading') {
+    return <div>Loading or not authenticated</div>;
+  }
+
   return <div>{children}</div>;
 }
