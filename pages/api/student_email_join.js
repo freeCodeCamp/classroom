398aa36bbd0de898c0d88073f7abe196ece40d16
EPeralta18 <eperalta@csumb.edu>
2022-04-14 21:15:28 -0700

    Able to update classroom students

---
+++ pages/api/student_email_join.js
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handle(req, res) {
+  const data = JSON.parse(req.body);
+  console.log(data);
+
+  const createdStudentEmail = await prisma.classroom.update({
+    where: {
+      classroomId: data['classId'][0]
+    },
+    data: {
+      fccUserIds: { push: data['email'] }
+    }
+  });
+
+  res.json(createdStudentEmail);
+}
