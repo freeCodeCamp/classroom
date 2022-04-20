4b91b8c04f46ec647e80c9117bc09545d59fb1aa
Guillermo Flores V <guillermo.fifth@gmail.com>
2022-04-20 10:39:08 -0700

    feat: Navbar fix and added table to classroom dashboard (#31)
    * Centered logo on our Navbar component
    
    * add dynamic columns
    
    * Working selector functions for dashboard table
    
    * got the /dashboard to correspond with a specific classroom URL
    
    * Added tabs to our classroom dashboard
    
    * Added conditional styling based on selected tab
    
    * Centered logo on our Navbar component
    
    * add dynamic columns
    
    * Working selector functions for dashboard table
    
    * got the /dashboard to correspond with a specific classroom URL
    
    * Added tabs to our classroom dashboard
    
    * Added conditional styling based on selected tab
    
    * cleanup and debug classroomIds
    
    * Rebase with main
    
    * add dynamic columns
    
    * Working selector functions for dashboard table
    
    * got the /dashboard to correspond with a specific classroom URL
    
    * Added tabs to our classroom dashboard
    
    * Added conditional styling based on selected tab
    
    * cleanup and debug classroomIds
    
    * Centered logo on our Navbar component
    
    * add dynamic columns
    
    * Working selector functions for dashboard table
    
    * got the /dashboard to correspond with a specific classroom URL
    
    * Added conditional styling based on selected tab
    
    * Refactored older navbar components & added rerouting when clicking on a class
    
    * Fix for CodeFactor checks
    
    Co-authored-by: Robert Huskins <37780741+roberthuskins@users.noreply.github.com>

---
+++ components/dashtabs.js
@@ -0,0 +1,53 @@
+import DashTable from './dashtable';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import reactTabStyles from './dashTabs.module.css';
+import { useState } from 'react/cjs/react.development';
+
+export default function DashTabs(props) {
+  const [tabIndex, setTabIndex] = useState(0);
+  // This sets our selected tab to our first index of certification module names
+  const [tabIndexName, setTabIndexName] = useState(props.certificationNames[0]);
+  // Here we are copying the columns array (which is now immutable) in order to be able to add the Student Name column to it
+  var columnNames = [...props.columns];
+  // The outer loop corresponds to each individual certification
+  for (let i = 0; i < columnNames.length; i += 1) {
+    let presetColumns = [];
+    presetColumns.push({ name: 'Student Name', selector: 'Student Name' });
+    columnNames[i] = presetColumns.concat(columnNames[i]);
+    // This inner loop adds the name of the column for each specific certification
+    for (let k in Object.keys(columnNames[i])) {
+      columnNames[i][k]['selector'] = row => row[columnNames[i][k]['name']];
+    }
+  }
+  // This function sets the tab name which later gives our selected tab selected styling
+  function determineItemStyle(x) {
+    setTabIndexName(x);
+  }
+
+  return (
+    <>
+      <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
+        <TabList>
+          {props.certificationNames.map(x => (
+            <Tab
+              onClick={() => determineItemStyle(x)}
+              className={
+                x == tabIndexName
+                  ? reactTabStyles.react_tabs__tab__selected
+                  : reactTabStyles.react_tabs__tab
+              }
+              key={x}
+            >
+              {x}
+            </Tab>
+          ))}
+        </TabList>
+        {columnNames.map(x => (
+          <TabPanel key={x}>
+            <DashTable columns={x} data={null}></DashTable>
+          </TabPanel>
+        ))}
+      </Tabs>
+    </>
+  );
+}
