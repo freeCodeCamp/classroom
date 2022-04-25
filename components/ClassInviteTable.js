2abcaa610799a318b22838b0ad332e56fc29cd58
Erik Gallardo-Cruz <erikgallard123@gmail.com>
2022-04-24 19:09:02 -0700

    edited layout.js and removed secure browser on joinpage

--- components/ClassInviteTable.js
+++ components/ClassInviteTable.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 
 export default function ClassInviteTable({ classes }) {
   const copy = async () => {
+    //Add the full URL to send to student
     await navigator.clipboard.writeText(classes.classroomId);
     alert(
       'Text copied for:' +
--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,3 @@
-import { useSession } from 'next-auth/react';
-
 export default function Layout({ children }) {
-  const { status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      return <div>onUnauthenticated</div>;
-    }
-  });
-
-  if (status === 'loading') {
-    return <div>Loading or not authenticated</div>;
-  }
-
   return <div>{children}</div>;
 }
