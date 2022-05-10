92eff98a6b9a534a2ccf5241baef3a94ec2ab94b
Guillermo Flores V <guillermo.fifth@gmail.com>
2022-05-09 17:59:07 -0700

    Switched dashtable code to functional programming solution

--- components/dashtable.js
+++ components/dashtable.js
@@ -1,6 +1,6 @@
 import DataTable from 'react-data-table-component';
 
-//intersection of 2 sets in O(n) time from MDN docs
+//from MDN docs
 function intersection(setA, setB) {
   let _intersection = new Set();
   for (let elem of setB) {
@@ -11,53 +11,38 @@ function intersection(setA, setB) {
   return _intersection;
 }
 
-//example of how student data is supposed to look like when passed into the table
-// const old = [
-//   {
-//     id: 1,
-//     'student-name': 'Guillermo',
-//     'basic-html-and-html5': '10/10',
-//     'basic-css': '0/10',
-//     'css-flexbox': '10/10',
-//     'css-grid': '7/10',
-//     'regular-expressions': '0/10',
-//     debugging: '0/10',
-//     'basic-data-structures': '4/10',
-//     'basic-algorithm-scripting': '10/10'
-//   },
-//   {
-//     id: 2,
-//     'student-name': 'Robert',
-//     'basic-html-and-html5': '10/10',
-//     'basic-css': '0/10',
-//     'css-flexbox': '10/10',
-//     'css-grid': '7/10',
-//     'regular-expressions': '0/10',
-//     debugging: '0/10',
-//     'basic-data-structures': '4/10',
-//     'basic-algorithm-scripting': '10/10'
-//   }
-// ];
-
 export default function DashTable(props) {
-  let output = [];
-
-  for (let i = 0; i < props.data.length; i++) {
-    let values = {};
-    values.map;
-    let studentName = Object.keys(props.data[i])[0];
-    for (const x of props.columns) {
-      if (x.selector !== 'student-name') {
-        values[x.selector] = intersection(
-          new Set(x.allChallenges),
-          new Set(props.data[i][studentName])
-        ).size;
-      } else {
-        values[x.selector] = studentName;
-      }
+  let output = Object.entries(props.columns).map(([i]) => {
+    if (props.data[i] != null) {
+      let studentName = Object.keys(props.data[Number(i)])[0];
+      let userOutput = {};
+      userOutput['id'] = Number(i) + 1;
+      // This loops through all out columns and populates the data accordingly. Because we are looping through the columns, we will have 'props.columns.length' objects inside of values, but we will filter them out later.
+      let values = props.columns.map(x => {
+        if (x.selector !== 'student-name') {
+          let intersectionSize = intersection(
+            new Set(x.allChallenges),
+            new Set(props.data[i][studentName])
+          ).size;
+          userOutput[
+            x.selector
+          ] = `${intersectionSize}/${x.allChallenges.length}`;
+        } else {
+          userOutput[x.selector] = studentName;
+        }
+        // This returns our user object when it is completed, otherwise we return undefined to our values array (which we will filter out later)
+        if (Object.keys(userOutput).length == props.columns.length) {
+          return userOutput;
+        }
+      });
+      // Filters out undefined parts of values
+      values = values.filter(obj => typeof obj == 'object');
+      return values;
     }
-    output.push(values);
-  }
-
+  });
+  // Filters out any possible undefined objs
+  output = output.filter(obj => typeof obj == 'object');
+  //turns our output array to a 1D array instead of 2D
+  output = output.flat(1);
   return <DataTable columns={props.columns} data={output} pagination />;
 }
