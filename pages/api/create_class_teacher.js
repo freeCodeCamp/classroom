2a86338bc2d43cdef1f8f67138745ff97d6fe775
Stephanie Hernandez <stephernandez831@gmail.com>
2022-10-02 11:05:10 -0700

    Added authentication to apis

--- pages/api/create_class_teacher.js
+++ pages/api/create_class_teacher.js
@@ -1,6 +1,33 @@
 import prisma from '../../prisma/prisma';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from './auth/[...nextauth]';
 
 export default async function handle(req, res) {
+  //unstable_getServerSession is recommended here: https://next-auth.js.org/configuration/nextjs
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!req.method == 'POST') {
+    res.status(405).end();
+  }
+
+  if (!session) {
+    res.status(403).end();
+  }
+
+  let users = await prisma.user.findMany({
+    where: {
+      role: 'TEACHER'
+    },
+    select: {
+      email: true
+    }
+  });
+
+  //if user is not teacher, reject request
+  if (!users.map(x => x.email).includes(session.user.email)) {
+    res.status(403).end();
+  }
+
   const data = JSON.parse(req.body);
 
   const createClassInDB = await prisma.classroom.create({
--- pages/api/student_email_join.js
+++ pages/api/student_email_join.js
@@ -1,7 +1,26 @@
 import prisma from '../../prisma/prisma';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from './auth/[...nextauth]';
 
 export default async function handle(req, res) {
+  //unstable_getServerSession is recommended here: https://next-auth.js.org/configuration/nextjs
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!req.method == 'POST') {
+    res.status(405).end();
+  }
+
+  if (!session) {
+    res.status(403).end();
+  }
+
   const data = JSON.parse(req.body);
+
+  //make sure user is adding themselves to a classroom
+  if (session.user.email !== data['email']) {
+    res.status(403).end();
+  }
+
   const userInfoReq = await fetch(
     `http://localhost:3002/getstudentprofile?email=${data['email']}`
   );
