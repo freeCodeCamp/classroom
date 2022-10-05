f8538b4fb270b80bf07e56458dcaf32e4621ccfb
Stephanie Hernandez <stephernandez831@gmail.com>
2022-10-04 21:26:00 -0700

    Changed to pull from database

--- pages/api/student_email_join.js
+++ pages/api/student_email_join.js
@@ -14,43 +14,38 @@ export default async function handle(req, res) {
     res.status(403).end();
   }
 
+  let user = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: session.user.email
+    }
+  });
+
   const data = JSON.parse(req.body);
 
   //make sure user is adding themselves to a classroom
-  if (session.user.email !== data['email']) {
+  if (user.email !== data['email']) {
     res.status(403).end();
   }
 
-  const userInfoReq = await fetch(
-    `http://localhost:3002/getstudentprofile?email=${data['email']}`
-  );
-  const userInfo = await userInfoReq.json();
-  if (userInfo.length !== 0) {
-    const existingStudent = await prisma.classroom.findUniqueOrThrow({
-      where: {
-        classroomId: data['classId'][0]
-      }
-    });
-    const ids = existingStudent['fccUserIds'];
-    if (ids.includes(userInfo[0]['uuid'])) {
-      return res.status(409).json({
-        error: 1,
-        msg: 'Account already exists'
-      });
+  const existingStudent = await prisma.classroom.findUniqueOrThrow({
+    where: {
+      classroomId: data['classId'][0]
     }
-    const createdStudentEmail = await prisma.classroom.update({
-      where: {
-        classroomId: data['classId'][0]
-      },
-      data: {
-        fccUserIds: { push: userInfo[0]['uuid'] }
-      }
-    });
-    return res.json(createdStudentEmail);
-  } else {
-    return res.status(400).json({
+  });
+  const ids = existingStudent['fccUserIds'];
+  if (ids.includes(user.id)) {
+    return res.status(409).json({
       error: 1,
-      msg: 'Not a freeCodeCamp email'
+      msg: 'Account already exists'
     });
   }
+  const createdStudentEmail = await prisma.classroom.update({
+    where: {
+      classroomId: data['classId'][0]
+    },
+    data: {
+      fccUserIds: { push: user.id }
+    }
+  });
+  return res.json(createdStudentEmail);
 }
