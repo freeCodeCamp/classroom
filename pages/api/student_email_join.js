06c347e7efd677382c17f05c6e34380e11a6bf33
Guillermo Flores V <guillermo.fifth@gmail.com>
2022-10-26 12:24:35 -0700

    fix: Join code refactored for students (#142)
    * join refactored
    
    * Fixed Firefox CORS issue
    
    * fix: got rid of unnecessary routing upon unsuccessful classroom join
    
    * Refactored join code to fit new JSON format
    
    * remove <h1> tag showing class code in UI
    
    * Removed empty else if statement and added a TODO instead
    
    Co-authored-by: Erik Gallardo-Cruz <erikgallard123@gmail.com>
    Co-authored-by: Utsab Saha <utsab.k.saha@gmail.com>

--- pages/api/student_email_join.js
+++ pages/api/student_email_join.js
@@ -1,37 +1,63 @@
 import prisma from '../../prisma/prisma';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from './auth/[...nextauth]';
 
 export default async function handle(req, res) {
-  const data = JSON.parse(req.body);
-  const userInfoReq = await fetch(
-    `http://localhost:3002/getstudentprofile?email=${data['email']}`
-  );
-  const userInfo = await userInfoReq.json();
-  if (userInfo.length !== 0) {
-    const existingStudent = await prisma.classroom.findMany({
-      where: {
-        classroomId: data['classId'][0]
-      }
-    });
-    const ids = existingStudent[0]['fccUserIds'];
-    if (ids.includes(userInfo[0]['uuid'])) {
-      return res.status(409).json({
-        error: 1,
-        msg: 'Account already exists'
-      });
+  // unstable_getServerSession is recommended here: https://next-auth.js.org/configuration/nextjs
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!req.method == 'PUT') {
+    res.status(405).end();
+  }
+
+  if (!session) {
+    res.status(403).end();
+  }
+
+  const body = req.body;
+  // Grab user info here
+  const userInfo = await prisma.user.findUnique({
+    where: {
+      email: session.user.email
+    },
+    select: {
+      id: true
     }
-    const createdStudentEmail = await prisma.classroom.update({
+  });
+  // Grab class info here
+  const checkClass = await prisma.classroom.findUniqueOrThrow({
+    where: {
+      classroomId: body.join[0]
+    },
+    select: {
+      fccUserIds: true
+    }
+  });
+  const existsInClassroom = checkClass.fccUserIds.includes(userInfo.id);
+  if (existsInClassroom) {
+    res.status(409).end();
+  }
+  // TODO: Once we allow multiple teachers inside of a classroom, make sure that the teachers
+  // are placed inside of the teacher array rather than as a regular student
+  else if (userInfo.role === 'NONE') {
+    // This runs only when a new user attempts to join a classroom.
+    await prisma.user.update({
       where: {
-        classroomId: data['classId'][0]
+        email: session.user.email
       },
       data: {
-        fccUserIds: { push: userInfo[0]['uuid'] }
+        role: 'STUDENT'
       }
     });
-    return res.json(createdStudentEmail);
-  } else {
-    return res.status(400).json({
-      error: 1,
-      msg: 'Not a freeCodeCamp email'
-    });
   }
+  // Update calssroom with user id
+  await prisma.classroom.update({
+    where: {
+      classroomId: body.join[0]
+    },
+    data: {
+      fccUserIds: { push: userInfo.id }
+    }
+  });
+  res.status(200).end();
 }
