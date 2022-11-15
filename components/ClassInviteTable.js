a04608f6778ef3d2bde0b5604cba10266a8d2e63
yashnerkar <yashnerkar512@gmail.com>
2022-11-15 21:30:18 +0530

    fix:added domain name in the url

--- components/ClassInviteTable.js
+++ components/ClassInviteTable.js
@@ -16,11 +16,12 @@ export default function ClassInviteTable({
   const [formData, setFormData] = useState({});
   const [selected, setSelected] = useState([]);
   const ref = useRef();
+  const api = process.env.NEXTAUTH_URL;
 
   const copy = async () => {
     //Add the full URL to send to student
     await navigator.clipboard.writeText(
-      '/join/' + classes.classroomId
+      `${api}/join/` + classes.classroomId
     );
 
     toast('Class code successfully copied', {
