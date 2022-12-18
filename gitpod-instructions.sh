7c927210a7033439fc0578df5cb4dba4e29f10ed
Shawn <stemp001@ucr.edu>
2022-12-18 19:18:59 +0000

    Second revision to gitpod instructions.,
    Co-authored-by: Qianru Wei <qwei5@dons.usfca.edu>
    Co-authored-by: Vanessa Valle <vallevanessa13@gmail.com>

--- gitpod-instructions.sh
+++ gitpod-instructions.sh
@@ -16,22 +16,19 @@ echo
 echo 2.\) Set NEXTAUTH_URL in .env to https://3000-${GITPOD_WORKSPACE_URL:8}
 echo
 echo 3.\) To setup GITHUB_ID and GITHUB_SECRET after setting up the OAuth app: 
-echo      -Your Client ID is shown in the OAuth app page. Copy and paste to GitHub_ID. 
-echo      -Next generate a new Client Secret. Then copy and paste to GitHub_Secret.
+echo      -Your Client ID is shown in the OAuth app page. Copy and paste to GITHUB_ID. 
+echo      -Next generate a new Client Secret. Then copy and paste to GITHUB_SECRET.
 echo
 echo Additional Steps to take After Finishing Gitpod Setup:
 echo
 echo 1.\) Run: npm run develop
-echo      -This will run the application.
 echo
 echo 2.\) Check to see if the website is functional and you can sign in and authorize yourself using your GitHub profile.
 echo
 echo 3.\) After successfully signing in, sign out and open a new terminal.
 echo
 echo 4.\) Run: npx prisma studio
-echo      -Follow the localhost link.
 echo
 echo 5.\) In Prisma Studio, go to User. Under the “roles” column, enter TEACHER or ADMIN. And click on the “Save Changes” button.
-echo      -Rerun application to test new privileges.
 echo
 echo Happy coding!
