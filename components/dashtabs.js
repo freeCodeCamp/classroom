67895de1c17e36d61f6af3f72b75c1bac7ba672d
Guillermo Flores V <guillermo.fifth@gmail.com>
2023-01-22 12:36:06 -0800

    fix: Classroom dashboard now uses mock API (#227)
    * use new fcc data
    
    * promises
    
    * refactored creating dashboard objs
    
    * Tabs and tab panels are now even
    
    * Working Dashboard integrated with mock API
    
    * Selectors in DashTable are now functions
    
    * removed filtering of student data
    
    * fix package-lock
    
    Co-authored-by: Robert Huskins <robertchuskins@gmail.com>
    Co-authored-by: Utsab Saha <utsab.k.saha@gmail.com>

--- components/dashtabs.js
+++ components/dashtabs.js
@@ -9,22 +9,17 @@ export default function DashTabs(props) {
   const [tabIndexName, setTabIndexName] = useState(props.certificationNames[0]);
   // Here we are copying the columns array (which is now immutable) in order to be able to add the Student Name column to it
   var columnNames = [...props.columns];
-
+  const presetColumns = [
+    {
+      name: 'Student Name',
+      selector: row => row['student-name'],
+      dashedName: 'student-name'
+    }
+  ];
   let columns = columnNames.map(x => {
-    let column = x.map(x => {
-      let column2 = x.map(x => {
-        return x;
-      });
-      let presetColumns = [{ name: 'Student Name', selector: 'student-name' }];
-      presetColumns = presetColumns.concat(column2);
-      return presetColumns;
-    });
-    return column;
+    let finalColumns = presetColumns.concat(x);
+    return finalColumns;
   });
-
-  // This reduces the columns array from 3D to a 2D array. 3D array creation occured inside of [id].js when we were mapping everything.
-  // Not necessarily a good long term fix, will look into ways of shortening it inside of [id].js
-  columns = columns.flat(1);
 
   // This function sets the tab name which later gives our selected tab selected styling
   function determineItemStyle(x) {
@@ -35,7 +30,7 @@ export default function DashTabs(props) {
     <>
       <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
         <TabList>
-          {props.certificationNames.map(x => (
+          {props.certificationNames.map((x, index) => (
             <Tab
               onClick={() => determineItemStyle(x)}
               className={
@@ -43,15 +38,23 @@ export default function DashTabs(props) {
                   ? reactTabStyles.react_tabs__tab__selected
                   : reactTabStyles.react_tabs__tab
               }
-              key={x}
+              key={index}
             >
               {x}
             </Tab>
           ))}
         </TabList>
-        {columns.map(x => (
-          <TabPanel key={x}>
-            <DashTable columns={x} data={props.data}></DashTable>
+        {/*
+          Here, we are mapping the columns array that holds our challenge names. These names are the columns of their own respective tables.
+          We do not want to send unnecessary column names to our table so we are splitting them here by certification using 
+          Tabs and Tablist to ensure they are sent to their respective Tab (Certification) 
+        */}
+        {columns.map(certification => (
+          <TabPanel key={certification}>
+            <DashTable
+              columns={certification}
+              data={props.studentData}
+            ></DashTable>
           </TabPanel>
         ))}
       </Tabs>
