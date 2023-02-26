cff3e9d6508a616873b1758e0265c41c74cefc75
Robert <robertchuskins@gmail.com>
2023-02-26 13:51:59 -0800

    remove changes to index.js

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,7 @@ export default function Home() {
           </div>
         </Navbar>
         <div className={styles.boxx}>
-          <h1
-            role='landing'
-            className='text-[40px] text-center big-heading underline'
-          >
+          <h1 className='text-[40px] text-center big-heading underline'>
             Welcome to FreeCodeCamp Classroom Mode
           </h1>
         </div>
