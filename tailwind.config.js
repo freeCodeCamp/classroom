520836a41ee18dadef646bf5aec7132cf6221170
HyperDanisH <93310647+HyperDanisH@users.noreply.github.com>
2023-04-19 13:42:03 +0530

    refactor(code-refactor): :sparkles: Refactored code to resolve #292 issue
    I have added colors to the tailwind.config.js file, now we can access the values without having to hardcode them each time. We can still have all the screen sizes, font sizes etc there but we will begin by colors only. I thought this could be a right fix. I will appericiate any review on this.
    
    #292

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,36 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  theme: {},
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: {
+        0o0: '#ffffff',
+        0o5: '#f5f6f7',
+        10: '#dfdfe2',
+        15: '#d0d0d5',
+
+        // DARK
+        75: '#3b3b4f',
+        80: '#2a2a40',
+        85: '#1b1b32',
+        90: '#0a0a23'
+      },
+      primary: {
+        purple: '#dbb8ff',
+        yellow: '#f1be32',
+        blue: '#99c9ff',
+        lightGreen: '#acd157'
+      },
+      secondary: {
+        darkPurple: '#5a01a7',
+        darkYellow: '#4d3800',
+        darkBlue: '#002ead',
+        darkGreen: '#00471b'
+      }
+    }
+  },
   extend: {
     colors: {
       'gray-90': '#0a0a23'
