df9e84988b3e9b2ed6f07ee428cfa094dc739006
Qianru69 <qwei5@dons.usfca.edu>
2023-04-20 18:17:33 +0000

    Check if user ID matches the classroom teacher ID
    Co-authored-by: Shawn Templer <stemp001@ucr.edu>
    Co-authored-by: Vanessa Valle <vallevanessa13@gmail.com>

--- pages/dashboard/v2/[id].js
+++ pages/dashboard/v2/[id].js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout from '../../../components/layout';
 import Link from 'next/link';
+import prisma from '../../prisma/prisma';
 import Navbar from '../../../components/navbar';
 import { getSession } from 'next-auth/react';
 
@@ -9,6 +10,30 @@ export async function getServerSideProps(context) {
   const userSession = await getSession(context);
   if (!userSession) {
     context.res.writeHead(302, { Location: '/' });
+    context.res.end();
+    return {};
+  }
+
+  const userEmail = await prisma.User.findMany({
+    where: {
+      email: userSession['user']['email']
+    }
+  });
+
+  const classroomTeacherId = await prisma.classroom.findUnique({
+    where: {
+      classroomId: context.params.id
+    },
+    select: {
+      classroomTeacherId: true
+    }
+  });
+
+  if (
+    classroomTeacherId == null ||
+    userEmail[0].id !== classroomTeacherId['classroomTeacherId']
+  ) {
+    context.res.writeHead(302, { Location: '/classes' });
     context.res.end();
     return {};
   }
