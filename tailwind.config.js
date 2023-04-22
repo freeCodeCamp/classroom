075e5d9e14175d9c3e66fa2246d9ebcd59ed8b9e
HyperDanisH <93310647+HyperDanisH@users.noreply.github.com>
2023-04-22 12:18:52 +0530

    Fix: Fixed a but that causes default tailwind err.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -38,7 +40,8 @@ module.exports = {
           darkBlue: '#002ead',
           darkGreen: '#00471b'
         }
-      }
+      },
+      ...colors
     }
   },
   extend: {
