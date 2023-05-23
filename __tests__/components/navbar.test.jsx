736c8042c867ef19c06224f163bab3196563f323
Ismail Tlemcani <34961373+Ismailtlem@users.noreply.github.com>
2023-05-23 22:28:34 +0200

    Update __tests__/components/__snapshots__/navbar.test.jsx.snap
    Co-authored-by: Guillermo Flores V <guillermo.fifth@gmail.com>

--- __tests__/components/navbar.test.jsx
+++ __tests__/components/navbar.test.jsx
@@ -1,15 +1,17 @@
 import Navbar from '../../components/navbar';
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 describe('Navbar rendering correctly', () => {
   it('renders correctly', () => {
-    const component = render(
-      <SessionProvider session={{ user: { name: 'test user' } }}>
-        <Navbar />
-      </SessionProvider>
-    );
-    expect(component).toMatchSnapshot();
+    const tree = renderer
+      .create(
+        <SessionProvider session={{ user: { name: 'test user' } }}>
+          <Navbar />
+        </SessionProvider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
