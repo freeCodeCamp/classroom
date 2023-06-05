570376352f0bbfb5d893a0f78f481be5c22493c5
dhruvspathak <91879998+dhruvspathak@users.noreply.github.com>
2023-06-05 12:40:04 +0400

    defined userID

--- components/dashtable_v2.js
+++ components/dashtable_v2.js
@@ -66,6 +66,7 @@ export default function GlobalDashboardTable(props) {
       name: studentName,
       activity: studentActivity,
       progress: percentageCompletion,
+      id: userID,
       detail: (
         <a
           // TODO:
