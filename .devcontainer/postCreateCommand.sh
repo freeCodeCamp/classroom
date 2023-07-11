a0a6e1313731ecce380aedebbf4abd89f28bd852
lloydchang <lloydchang@gmail.com>
2023-07-11 17:00:14 +0000

    build(.devcontainer), ci(package.json): pin to Node.js version 16
    Be at parity with GitPod

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,11 +3,19 @@ export NVM_DIR="/usr/local/share/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cp --no-clobber .env.sample .env
-nvm install --lts
-nvm alias default 'lts/*'
-# Uncomment to use Node.js 16.20.0
-# nvm install 16.20.0
-# nvm alias default '16.20.0'
+
+# Uncomment to use lts (long-term support)
+# nvm install --lts
+# nvm alias default 'lts/*'
+
+# Use Node.js 18.16.1
+# nvm install 18.16.1
+# nvm alias default '18.16.1'
+
+# Use Node.js 16.20.0
+nvm install 16.20.0
+nvm alias default '16.20.0'
+
 nvm use default
 npm ci --prefer-offline --no-audit --no-progress
 npx prisma generate
