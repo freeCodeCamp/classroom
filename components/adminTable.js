074275abfeac9eae8b36dea7ee45563eb1720c55
Komal Kaur <komalkaurstaymenity@gmail.com>
2023-07-26 22:41:28 -0700

    fix: react hydration errors on admin

--- components/adminTable.js
+++ components/adminTable.js
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import DataTable from 'react-data-table-component';
+import dynamic from 'next/dynamic';
 
-export default function AdminTable(props) {
+function AdminTable(props) {
   let users = props.data.map(i => {
     let userObj = {};
     userObj['name'] = i.name;
@@ -15,3 +16,5 @@ export default function AdminTable(props) {
 
   return <DataTable columns={props.columns} data={users} pagination />;
 }
+
+export default dynamic(() => Promise.resolve(AdminTable), { ssr: false });
