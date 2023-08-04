2c79eca18515a23ef0fc049764fdd31014d588f8
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-03 20:13:36 -0700

    fix?: used jest.spyOn to mock Date.getTime()
    Co-authored-by: Komal <kaur91499@gmail.com>

--- __tests__/components/dashtable_v2.test.jsx
+++ __tests__/components/dashtable_v2.test.jsx
@@ -4,8 +4,13 @@ import GlobalDashboardTable from '../../components/dashtable_v2.js';
 
 import {studentData, certifications, classroomId} from '../../testing_data/testing-data';
 
-
 describe('GlobalDashboardTable', () => {
+  // Define a fixed time in milliseconds
+  const fixedTime = new Date('2021-01-01T12:00:00').getTime();
+
+  // Spy on Date.prototype.getTime and mock its implementation
+  const getTimeSpy = jest.spyOn(Date.prototype, 'getTime').mockImplementation(() => fixedTime);
+
   test('renders dashtable with correct data format', () => {
     const { container } = render(
       <GlobalDashboardTable
@@ -16,5 +21,10 @@ describe('GlobalDashboardTable', () => {
     );
 
     expect(container).toMatchSnapshot();
+  });
+
+  // Restore the original getTime method after the test
+  afterAll(() => {
+    getTimeSpy.mockRestore();
   });
 });
