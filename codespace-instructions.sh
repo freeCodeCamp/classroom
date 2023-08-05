528516a77acd89d87eaae2d98e63d49d22a0e30c
lloydchang <lloydchang@gmail.com>
2023-08-05 04:41:08 +0000

    build(codespace-instructions.sh): remove "preview." from Codespaces URL
    GitHub removed it
    
    See https://github.com/freeCodeCamp/classroom/pull/436

--- codespace-instructions.sh
+++ codespace-instructions.sh
@@ -10,15 +10,15 @@ Almost done! Just need to set up a NextAuth Provider and run the app.
 https://docs.github.com/en/developers/apps/building-oauth-apps/creating-an-oauth-app
 
 The mock user data URL is:
-https://${CODESPACE_NAME}-3001.preview.app.github.dev/data
+https://${CODESPACE_NAME}-3001.app.github.dev/data
 
 The homepage is:
-https://${CODESPACE_NAME}-3000.preview.app.github.dev
+https://${CODESPACE_NAME}-3000.app.github.dev
 
 The callback URL is:
-https://${CODESPACE_NAME}-3000.preview.app.github.dev/api/auth/callback/github
+https://${CODESPACE_NAME}-3000.app.github.dev/api/auth/callback/github
 
-2.) Set NEXTAUTH_URL in .env to https://${CODESPACE_NAME}-3000.preview.app.github.dev
+2.) Set NEXTAUTH_URL in .env to https://${CODESPACE_NAME}-3000.app.github.dev
 
 3.) To setup GITHUB_ID and GITHUB_SECRET after setting up the OAuth app:
 -Your Client ID is shown in the OAuth app page. Copy and paste it to GITHUB_ID.
