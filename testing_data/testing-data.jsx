413a66ad195809a3d0c803477d1b324b152c9f83
Hamzat Victor Oluwabori <oluwaborihamzat@gmail.com>
2023-08-15 13:30:00 +0000

    feat: create snapshot test for classInviteTable
    Co-authored-by: Ayomide Onifade <onifadeayomide328@gmail.com>
    Co-authored-by: Christopher Alade <aladechristoph@gmail.com>
    Co-authored-by: Quawiyyah Abdulhameed <abdulhameedqo@gmail.com>

--- testing_data/testing-data.jsx
+++ testing_data/testing-data.jsx
@@ -1,137 +1,136 @@
 export const studentData = [
-    {
-      "email": "student[A]@gmail.com",
-      "certifications": [
-        {
-          "2022/responsive-web-design": {
-            "blocks": [
-              {
-                "learn-basic-css-by-building-a-cafe-menu": {
-                  "completedChallenges": [
-                    {
-                      "id": "5f33071498eb2472b87ddee4",
-                      "challengeName": "Step 1",
-                      "completedDate": 1475094716730,
-                      "files": []
-                    },
-                    {
-                      "id": "5f3313e74582ad9d063e3a38",
-                      "challengeName": "Step 2",
-                      "completedDate": 1537207306322,
-                      "files": []
-                    }
-                  ]
-                }
+  {
+    email: 'student[A]@gmail.com',
+    certifications: [
+      {
+        '2022/responsive-web-design': {
+          blocks: [
+            {
+              'learn-basic-css-by-building-a-cafe-menu': {
+                completedChallenges: [
+                  {
+                    id: '5f33071498eb2472b87ddee4',
+                    challengeName: 'Step 1',
+                    completedDate: 1475094716730,
+                    files: []
+                  },
+                  {
+                    id: '5f3313e74582ad9d063e3a38',
+                    challengeName: 'Step 2',
+                    completedDate: 1537207306322,
+                    files: []
+                  }
+                ]
               }
-            ]
-          }
-        },
-        {
-          "quality-assurance": {
-            "blocks": [
-              {
-                "advanced-node-and-express": {
-                  "completedChallenges": [
-                    {
-                      "id": "5895f700f9fc0f352b528e63",
-                      "challengeName": "Set up a Template Engine",
-                      "completedDate": 98448684,
-                      "files": []
-                    },
-                    {
-                      "id": "5895f70df9fc0f352b528e6a",
-                      "challengeName": "Create New Middleware",
-                      "completedDate": 98448643284,
-                      "files": []
-                    }
-                  ]
-                }
-              },
-              {
-                "quality-assurance-and-testing-with-chai": {
-                  "completedChallenges": [
-                    {
-                      "id": "587d824a367417b2b2512c46",
-                      "challengeName": "Learn How JavaScript Assertions Work",
-                      "completedDate": 47664591,
-                      "files": []
-                    }
-                  ]
-                }
-              }
-            ]
-          }
+            }
+          ]
         }
-      ]
-    },
-    {
-      "email": "student[B]@gmail.com",
-      "certifications": [
-        {
-          "quality-assurance": {
-            "blocks": [
-              {
-                "advanced-node-and-express": {
-                  "completedChallenges": [
-                    {
-                      "id": "5895f700f9fc0f352b528e63",
-                      "challengeName": "Set up a Template Engine",
-                      "completedDate": 98448684,
-                      "files": []
-                    },
-                    {
-                      "id": "5895f70df9fc0f352b528e6a",
-                      "challengeName": "Create New Middleware",
-                      "completedDate": 98448643284,
-                      "files": []
-                    }
-                  ]
-                }
-              },
-              {
-                "quality-assurance-and-testing-with-chai": {
-                  "completedChallenges": [
-                    {
-                      "id": "587d824a367417b2b2512c46",
-                      "challengeName": "Learn How JavaScript Assertions Work",
-                      "completedDate": 47664591,
-                      "files": []
-                    }
-                  ]
-                }
+      },
+      {
+        'quality-assurance': {
+          blocks: [
+            {
+              'advanced-node-and-express': {
+                completedChallenges: [
+                  {
+                    id: '5895f700f9fc0f352b528e63',
+                    challengeName: 'Set up a Template Engine',
+                    completedDate: 98448684,
+                    files: []
+                  },
+                  {
+                    id: '5895f70df9fc0f352b528e6a',
+                    challengeName: 'Create New Middleware',
+                    completedDate: 98448643284,
+                    files: []
+                  }
+                ]
               }
-            ]
-          }
+            },
+            {
+              'quality-assurance-and-testing-with-chai': {
+                completedChallenges: [
+                  {
+                    id: '587d824a367417b2b2512c46',
+                    challengeName: 'Learn How JavaScript Assertions Work',
+                    completedDate: 47664591,
+                    files: []
+                  }
+                ]
+              }
+            }
+          ]
         }
-      ]
-    }
+      }
+    ]
+  },
+  {
+    email: 'student[B]@gmail.com',
+    certifications: [
+      {
+        'quality-assurance': {
+          blocks: [
+            {
+              'advanced-node-and-express': {
+                completedChallenges: [
+                  {
+                    id: '5895f700f9fc0f352b528e63',
+                    challengeName: 'Set up a Template Engine',
+                    completedDate: 98448684,
+                    files: []
+                  },
+                  {
+                    id: '5895f70df9fc0f352b528e6a',
+                    challengeName: 'Create New Middleware',
+                    completedDate: 98448643284,
+                    files: []
+                  }
+                ]
+              }
+            },
+            {
+              'quality-assurance-and-testing-with-chai': {
+                completedChallenges: [
+                  {
+                    id: '587d824a367417b2b2512c46',
+                    challengeName: 'Learn How JavaScript Assertions Work',
+                    completedDate: 47664591,
+                    files: []
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    ]
+  }
 ];
 export const certifications = [
-    [
-      {
-        name: "Learn HTML By Building a Cat Photo App",
-        selector: "learn-html-by-building-a-cat-photo-app",
-        dashedName: "learn-html-by-building-a-cat-photo-app",
-        allChallenges: [
-          "5dc174fcf86c76b9248c6eb2",
-          "5dc1798ff86c76b9248c6eb3",
-          // ... other challenge IDs
-          "5ef9b03c81a63668521804ec",
-          "5ef9b03c81a63668521804ee",
-          "62bb4009e3458a128ff57d5d",
-        ],
-        order: 0,
-      },
-      // ... other certifications
-      {
-        name: "Build a Personal Portfolio Webpage Project",
-        selector: "build-a-personal-portfolio-webpage-project",
-        dashedName: "build-a-personal-portfolio-webpage-project",
-        allChallenges: [
-          "bd7158d8c242eddfaeb5bd13",
-        ],
-        order: 19,
-      },
-    ]
+  [
+    {
+      name: 'Learn HTML By Building a Cat Photo App',
+      selector: 'learn-html-by-building-a-cat-photo-app',
+      dashedName: 'learn-html-by-building-a-cat-photo-app',
+      allChallenges: [
+        '5dc174fcf86c76b9248c6eb2',
+        '5dc1798ff86c76b9248c6eb3',
+        // ... other challenge IDs
+        '5ef9b03c81a63668521804ec',
+        '5ef9b03c81a63668521804ee',
+        '62bb4009e3458a128ff57d5d'
+      ],
+      order: 0
+    },
+    // ... other certifications
+    {
+      name: 'Build a Personal Portfolio Webpage Project',
+      selector: 'build-a-personal-portfolio-webpage-project',
+      dashedName: 'build-a-personal-portfolio-webpage-project',
+      allChallenges: ['bd7158d8c242eddfaeb5bd13'],
+      order: 19
+    }
+  ]
 ];
 export const classroomId = 'cljbqrfoa0006kohk894ym6v3';
+export const userId = 'cljyeiqeq0000e1k5fyxg20pa';
