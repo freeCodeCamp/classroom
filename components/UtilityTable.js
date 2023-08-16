48184ad14ddd18a937f40ad074c3c0c522091705
Natalia Gill <sngill.ca@gmail.com>
2023-08-16 21:53:08 +0000

    Moving the table UI used with the useTable hook from the react table library in it's own component so it's reusable and the file is more readable.

---
+++ components/UtilityTable.js
@@ -0,0 +1,55 @@
+export default function Table(props) {
+  return (
+    <>
+      <table
+        {...props.getTableProps}
+        style={{ border: 'solid 1px #0a0a23', width: '100%', margin: 'auto' }}
+      >
+        <thead>
+          {props.headerGroups.map((headerGroup, index) => (
+            <tr {...headerGroup.getHeaderGroupProps()} key={index}>
+              {headerGroup.headers.map((column, index) => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    borderBottom: 'solid 3px grey',
+                    color: 'black',
+                    fontWeight: 'bold'
+                  }}
+                  key={index}
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...props.getTableBodyProps}>
+          {props.rows.map((row, index) => {
+            props.prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} key={index}>
+                {row.cells.map((cell, index) => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: '10px',
+                        border: 'solid 1px grey',
+                        textAlign: 'center',
+                        width: cell.column.width
+                      }}
+                      key={index}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
--- components/dashtable_v2.js
+++ components/dashtable_v2.js
@@ -1,4 +1,5 @@
 import { useTable } from 'react-table';
+import Table from './UtilityTable';
 import React from 'react';
 import getStudentActivity from './studentActivity';
 
@@ -140,55 +141,15 @@ export default function GlobalDashboardTable(props) {
 
   return (
     <>
-      <table
-        {...getTableProps()}
-        style={{ border: 'solid 1px #0a0a23', width: '100%', margin: 'auto' }}
-      >
-        <thead>
-          {headerGroups.map((headerGroup, index) => (
-            <tr {...headerGroup.getHeaderGroupProps()} key={index}>
-              {headerGroup.headers.map((column, index) => (
-                <th
-                  {...column.getHeaderProps()}
-                  style={{
-                    borderBottom: 'solid 3px grey',
-                    color: 'black',
-                    fontWeight: 'bold'
-                  }}
-                  key={index}
-                >
-                  {column.render('Header')}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, index) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()} key={index}>
-                {row.cells.map((cell, index) => {
-                  return (
-                    <td
-                      {...cell.getCellProps()}
-                      style={{
-                        padding: '10px',
-                        border: 'solid 1px grey',
-                        textAlign: 'center',
-                        width: cell.column.width
-                      }}
-                      key={index}
-                    >
-                      {cell.render('Cell')}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <Table
+        getTableProps={getTableProps()}
+        getTableBodyProps={getTableBodyProps()}
+        headerGroups={headerGroups}
+        prepareRow={prepareRow}
+        rows={rows}
+        columns={columns}
+        data={data}
+      ></Table>
     </>
   );
 }
