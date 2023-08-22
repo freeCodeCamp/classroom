4c2c0df8257d17b842586d02a30865ed8b47506b
Natalia Gill <sngill.ca@gmail.com>
2023-08-22 23:57:46 +0000

    Adding list of superblock titles the student is enrolled in on the details page with updated Details index.js page. Co-authored-by: Dmagical <mjonespacheco.mjp@gmail.com>

--- pages/dashboard/v2/[id].js
+++ pages/dashboard/v2/[id].js
@@ -65,12 +65,22 @@ export async function getServerSideProps(context) {
   let superBlockJsons = await getSuperBlockJsons(superblockURLS);
   let dashboardObjs = createDashboardObject(superBlockJsons);
 
+  const classroomName = await prisma.classroom.findUnique({
+    where: {
+      classroomId: context.params.id
+    },
+    select: {
+      classroomName: true
+    }
+  });
+
   return {
     props: {
       userSession,
       studentData: currStudentData,
       certifications: dashboardObjs,
-      classroomId: context.params.id
+      classroomId: context.params.id,
+      classroomName: classroomName.classroomName
     }
   };
 }
@@ -79,7 +89,8 @@ export default function Home({
   userSession,
   studentData,
   certifications,
-  classroomId
+  classroomId,
+  classroomName
 }) {
   return (
     <Layout>
@@ -102,6 +113,7 @@ export default function Home({
             studentData={studentData}
             certifications={certifications}
             classroomId={classroomId}
+            classroomName={classroomName}
           ></GlobalDashboardTable>
         </>
       )}
