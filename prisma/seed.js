e8ae4a073d24196a9e5550c4242c4f4c7897337e
Kandula Naveen <104118171+Kandulanaveennaidu@users.noreply.github.com>
2024-01-14 09:24:12 +0530

    Update seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,20 @@
 const { createClassrooms } = require('./factories/classroom');
 const { createUsers } = require('./factories/user');
 
-(async () => {
-  const users = await createUsers();
-  await createClassrooms(users);
-})();
+const main = async () => {
+  try {
+    console.log('Creating users...');
+    const users = await createUsers();
+    console.log('Users created successfully.');
+
+    console.log('Creating classrooms...');
+    await createClassrooms(users);
+    console.log('Classrooms created successfully.');
+  } catch (error) {
+    console.error('Error occurred:', error);
+  } finally {
+    console.log('Script execution completed.');
+  }
+};
+
+main();
